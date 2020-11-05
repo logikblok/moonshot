560a98196d7d60dd6f2705dbaf8ee1dabf1b3320
Suraj Rai <surajrajrai@gmail.com>
2020-11-05 22:23:53 +0000

    update

--- sketch.js
+++ sketch.js
@@ -1,27 +1,37 @@
-var x = 40;
-var xsize = false;
-
+//Moving sprites
+var earth, circle;
+var direction = 90; //circle initial direction moving down
 
 function setup() {
-createCanvas(windowWidth,windowHeight);
-background(0);
+  createCanvas(windowWidth, windowHeight);
+  //create the sprites
+  earth = createSprite(600, 200, 50, 100);
+  earth.addAnimation('floating', 'assets/earth_0001.png', 'assets/earth_0009.png');
+
+  circle = createSprite(400, 200, 50, 100);
+  circle.addAnimation('floating', 'assets/asterisk_circle0006.png', 'assets/asterisk_circle0008.png');
 }
 
 function draw() {
-    if(xsize) {
-    x = x + 10;
-    if (x > 90){
-      xsize = false;
-    }
-  } else {
-      x = x - 5;
-      if ( x < 10 ){
-        xsize = true;
-      }
-    }
-  ellipse(mouseX, mouseY, x,x);
-}
+  background('#24285B');
 
-function keyPressed(){
-background(0);
+  //aside of setting the velocity directly you can move a sprite
+  //by providing a speed and an angle
+  direction += 2;
+  //speed, angle
+  circle.setSpeed(3, direction);
+
+  //you can rotate the sprite according the direction it is moving
+  //uncomment this
+  //circle.rotateToDirection = true;
+
+  //or by applying a force toward a point
+  //force (acceleration), pointx, pointy
+  earth.attractionPoint(0.2, mouseX, mouseY);
+  //since the force keeps incrementing the speed you can
+  //set a limit to it with maxSpeed
+  earth.maxSpeed = 5;
+
+  //draw the sprite
+  drawSprites();
 }
