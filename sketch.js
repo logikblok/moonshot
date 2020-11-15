4f90357146d80d34f196600a29348bcb2a311f37
Suraj Rai <surajrajrai@gmail.com>
2020-11-15 20:09:54 +0000

    update

--- sketch.js
+++ sketch.js
@@ -1,34 +1,79 @@
-//Moving sprites
-var earth, circle;
-var direction = 90; //circle initial direction moving down
+
+var rocket;
+var bg;
+var frame;
+var thrust = 2;
+var debris;
+
+//the scene is space
+var SCENE_W = 1600;
+var SCENE_H = 800;
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  //create the sprites
-  earth = createSprite(600, 200, 50, 100);
-  earth.addAnimation('floating', 'assets/earth_0001.png', 'assets/earth_0009.png');
+  createCanvas(800, 400);
+  //create a sprite and add the 3 animations
+  rocket = createSprite(400, 200, 50, 50);
+  debris = createSprite(10, 20, 5, 5);
+
+  bg = new Group();
+  for (var i = 0; i < 100; i++) {
+    //create a sprite and add the 3 animations
+    var rock = createSprite(random(-width, SCENE_W + width), random(-height, SCENE_H + height));
+    //cycles through rocks 0 1 2
+    rock.addAnimation('normal', 'assets/favicon.png');
+    bg.add(rock);
+  }
+
+  //frame = loadImage('assets/frame.png');
 }
 
 function draw() {
   background('#24285B');
 
-  //aside of setting the velocity directly you can move a sprite
-  //by providing a speed and an angle
-  direction += 2;
-  //speed, angle
-  circle.setSpeed(3, direction);
+    if (keyWentDown(LEFT_ARROW)) {
+      rocket.velocity.x = -thrust;
+      rocket.setSpeed(10);
+    }
+    if (keyWentDown(RIGHT_ARROW)) {
+      rocket.velocity.x = +thrust;
+      rocket.setSpeed(10);
+    }
+    if (keyWentDown(UP_ARROW)) {
+      rocket.velocity.y = -thrust;
+      rocket.setSpeed(10);
+    }
+    if (keyWentDown(DOWN_ARROW)) {
+      rocket.velocity.y = +thrust;
+      rocket.setSpeed(10);
+    }
 
-  //you can rotate the sprite according the direction it is moving
-  //uncomment this
-  //circle.rotateToDirection = true;
+    //.5 zoom is zooming out (50% of the normal size)
+    if (mouseIsPressed)
+      camera.zoom = 0.5;
+    else
+      camera.zoom = 1;
 
-  //or by applying a force toward a point
-  //force (acceleration), pointx, pointy
-  earth.attractionPoint(0.2, mouseX, mouseY);
-  //since the force keeps incrementing the speed you can
-  //set a limit to it with maxSpeed
-  earth.maxSpeed = 5;
+    //set the camera position to the rocket position
+    camera.position.x = rocket.position.x;
+    camera.position.y = rocket.position.y;
 
-  //draw the sprite
-  drawSprites();
-}
+    //draw the scene
+    //rocks first
+    drawSprites(bg);
+
+    //shadow using p5 drawing
+    noStroke();
+    fill(0, 0, 0, 20);
+    //shadow
+    ellipse(rocket.position.x, rocket.position.y + 90, 80, 30);
+    //character on the top
+    drawSprite(rocket);
+    drawSprite(debris);
+    debris.attractionPoint(0.2, rocket.position.x, rocket.position.y);
+    //I can turn on and off the camera at any point to restore
+    //the normal drawing coordinates, the frame will be drawn at
+    //the absolute 0,0 (try to see what happens if you don't turn it off
+    camera.off();
+    //image(frame, 0, 0);
+  }
