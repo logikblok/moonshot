bcc69798494608064ef00f07e3af9fc20e605c92
Suraj Rai <surajrajrai@gmail.com>
2020-11-24 17:27:26 +0000

    moonshotprogress

--- sketch.js
+++ sketch.js
@@ -1,79 +1,63 @@
-
-var rocket;
-var bg;
-var frame;
+//Our key sprites for the game
+var rocket, ground, debris, moon,frame,boxSprite, bg;
 var thrust = 2;
-var debris;
-
+var JUMP = 15;
 //the scene is space
-var SCENE_W = 1600;
-var SCENE_H = 800;
+var SCENE_W = 600;
+var SCENE_H = 2000;
+var canvasWidth = 400;
+var canvasHeight = 650;
 
+function preload() {
+  //create an animation from a sequence of numbered images
+  rocket = loadAnimation('assets/rocket_0001.png', 'assets/rocket_0004.png');
+}
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(canvasWidth, canvasHeight);
   //create a sprite and add the 3 animations
-  rocket = createSprite(400, 200, 50, 50);
-  debris = createSprite(10, 20, 5, 5);
-
+  //debris = createSprite(10, 20, 5, 5);
+  moon = createSprite(170,-SCENE_H,1000,400);
+  ground = createSprite(170, 525,1000,400);
+  ground.shapeColor = color(127, 140, 141);
+  ground.setCollider('rectangle',0,100,1000,400);
+  //compact way to add an image
+  rocket = createSprite(canvasWidth/2,canvasHeight/2);
+  rocket.addAnimation('normal', 'assets/rocket_0001.png');
+  rocket.addAnimation('push', 'assets/rocket_0001.png', 'assets/rocket_0004.png');
+  //frame = loadImage('assets/frame.png');
   bg = new Group();
   for (var i = 0; i < 100; i++) {
-    //create a sprite and add the 3 animations
-    var rock = createSprite(random(-width, SCENE_W + width), random(-height, SCENE_H + height));
-    //cycles through rocks 0 1 2
-    rock.addAnimation('normal', 'assets/favicon.png');
-    bg.add(rock);
+    var stars1 = createSprite(random(-width, SCENE_W+width), random(-height, SCENE_H+height));
   }
-
-  //frame = loadImage('assets/frame.png');
 }
 
 function draw() {
   background('#24285B');
-
-    if (keyWentDown(LEFT_ARROW)) {
-      rocket.velocity.x = -thrust;
-      rocket.setSpeed(10);
-    }
-    if (keyWentDown(RIGHT_ARROW)) {
-      rocket.velocity.x = +thrust;
-      rocket.setSpeed(10);
-    }
-    if (keyWentDown(UP_ARROW)) {
-      rocket.velocity.y = -thrust;
-      rocket.setSpeed(10);
-    }
-    if (keyWentDown(DOWN_ARROW)) {
-      rocket.velocity.y = +thrust;
-      rocket.setSpeed(10);
-    }
-
-    //.5 zoom is zooming out (50% of the normal size)
-    if (mouseIsPressed)
-      camera.zoom = 0.5;
-    else
-      camera.zoom = 1;
-
-    //set the camera position to the rocket position
-    camera.position.x = rocket.position.x;
-    camera.position.y = rocket.position.y;
-
-    //draw the scene
-    //rocks first
-    drawSprites(bg);
-
-    //shadow using p5 drawing
-    noStroke();
-    fill(0, 0, 0, 20);
-    //shadow
-    ellipse(rocket.position.x, rocket.position.y + 90, 80, 30);
-    //character on the top
-    drawSprite(rocket);
-    drawSprite(debris);
-    debris.attractionPoint(0.2, rocket.position.x, rocket.position.y);
-    //I can turn on and off the camera at any point to restore
-    //the normal drawing coordinates, the frame will be drawn at
-    //the absolute 0,0 (try to see what happens if you don't turn it off
-    camera.off();
-    //image(frame, 0, 0);
+  noStroke();
+  fill(0, 0, 0, 20);
+  //set the camera position to the rocket position
+  //camera.position.x = rocket.position.x;
+  camera.position.y = rocket.position.y;
+  //draw the scene
+  //drawSprites(bg);
+  //character on the top
+  //debris.attractionPoint(0.2, rocket.position.x, rocket.position.y);
+  drawSprites(bg);
+  if (keyIsPressed === true) {
+    rocket.changeAnimation('push');
+    rocket.velocity.y = -JUMP;
+  } else {
+    rocket.changeAnimation('normal');
+    rocket.velocity.y = +JUMP;
   }
+  rocket.collide(ground);
+  rocket.debug = mouseIsPressed;
+  //I can turn on and off the camera at any point to restore
+  //the normal drawing coordinates, the frame will be drawn at
+  //the absolute 0,0 (try to see what happens if you don't turn it off
+  //image(frame, 0, 0);
+  drawSprites();
+  //ellipse(rocket.position.x, rocket.position.y+70, 80, 30);
+  camera.off();
+}
